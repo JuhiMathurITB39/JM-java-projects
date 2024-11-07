747d5210a4189add2d62d067454497d749894e24
JuhiMathurITB39 <juhimathur765@gmail.com>
2024-11-07 15:34:33 +0530

    Add files via upload

---
+++ Fahrenheit.java
@@ -0,0 +1,9 @@
+class Fahrenheit{
+    public static void main(String args[]){
+    double C=100.00,F=0.00;
+    F=(C*1.8)+32;        
+    System.out.println("Value of Fahrenheit" +F);
+
+    }
+
+}
